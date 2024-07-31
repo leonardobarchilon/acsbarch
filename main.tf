04df4f1594ff39b1480ba38b18ae3b9c94c1f4e2
leonardobarchilon <leonardobarchilon@gmail.com>
2024-07-31 20:03:34 -0300

    Test 8

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  shared_config_files = ["/home/barchilon/.aws/config"]
-  shared_credentials_files = ["/home/barchilon/.aws/credentials"]
-  region  = var.region
+#  shared_config_files = ["/home/barchilon/.aws/config"]
+#  shared_credentials_files = ["/home/barchilon/.aws/credentials"]
+#  region  = var.region
 
 #  profile = "default"
 #  assume_role {
