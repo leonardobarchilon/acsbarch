47639fa0b2c9e3eb9ee0af53ab1d741061397c25
leonardobarchilon <leonardobarchilon@gmail.com>
2024-07-31 22:30:36 -0300

    Validacao 10

--- helm-provider.tf
+++ helm-provider.tf
@@ -1,10 +1,10 @@
 provider "helm" {
     kubernetes {
-      host = data.aws_eks_cluster.this.endpoint
-      cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+      host = data.aws_eks_cluster.main.endpoint
+      cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
       exec {
         api_version = "client.authentication.k8s.io/v1beta1"
-        args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this.id]
+        args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.main.id]
         command     = "aws"
       }
  
